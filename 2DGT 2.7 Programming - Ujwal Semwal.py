6172e5acd051b9d75d6d5ab9fd4d220e3ab57796
usemwal-programming <23usemwal@rbhs.school.nz>
2026-09-23 09:57:52 +1200

    Refactor hire record management functions

--- 2DGT 2.7 Programming - Ujwal Semwal.py
+++ 2DGT 2.7 Programming - Ujwal Semwal.py
@@ -39,6 +39,8 @@ quantity = int(qty_text)
 try:
     start_dt = datetime.datetime.strptime(start_date, "%d/%m/%Y")
     return_dt = datetime.datetime.strptime(return_date, "%d/%m/%Y")
+     except ValueError:
+        return False, "Dates
 
 
     # this function checks the validity of the return date so it is not before the hire date
