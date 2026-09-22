9a07e0e418db7ee8b25be7e7ef6d4a5c18f66856
usemwal-programming <23usemwal@rbhs.school.nz>
2026-09-23 09:46:53 +1200

    Update 2DGT 2.7 Programming - Ujwal Semwal.py

--- 2DGT 2.7 Programming - Ujwal Semwal.py
+++ 2DGT 2.7 Programming - Ujwal Semwal.py
@@ -29,6 +29,7 @@ this function checks the validity of the receipt number so it only contains numb
         
 # this function checks the validity of the quantity so it is a whole number within limits
    if not qty_text.isdigit():
+       return False, "Quantity must be a valid whole number."
     
     # this function checks the validity of the return date so it is not before the hire date
     if return_dt < start_dt:
