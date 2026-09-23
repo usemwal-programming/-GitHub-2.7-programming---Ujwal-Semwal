80dfe3904bec7a91c692c506195ac12a6a215ba8
usemwal-programming <23usemwal@rbhs.school.nz>
2026-09-23 18:48:34 +1200

    Update 2DGT 2.7 Programming - Ujwal Semwal.py

--- 2DGT 2.7 Programming - Ujwal Semwal.py
+++ 2DGT 2.7 Programming - Ujwal Semwal.py
@@ -60,7 +60,7 @@ def clear_placeholder(event, entry_box):
         entry_box.config(fg="black")
 
 def restore_placeholder(event, entry_box):
-     # this function brings back the placeholder
+     # this function brings back the placeholder text if the entry box is left empty
     if not entry_box.get().strip():
         set_placeholder(entry_box)
 
