4acf32477efda274cd58af12804137fa8718bb3f
usemwal-programming <23usemwal@rbhs.school.nz>
2026-09-23 18:51:05 +1200

    Restore import statements and function definitions

--- 2DGT 2.7 Programming - Ujwal Semwal.py
+++ 2DGT 2.7 Programming - Ujwal Semwal.py
@@ -70,6 +70,7 @@ def add_hire_record():
     receipt = entry_receipt.get().strip()
     item = entry_item.get().strip()
     qty_text = entry_quantity.get().strip()
+    start_date = entry_start.get().strip()
 
 
     # this function runs the validation subroutine and checks if inputs passed validity
