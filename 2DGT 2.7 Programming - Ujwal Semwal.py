434bd9e92d10c180ead0d01cc126f64a052a5bd7
usemwal-programming <23usemwal@rbhs.school.nz>
2026-09-24 10:27:11 +1200

    Update 2DGT 2.7 Programming - Ujwal Semwal.py

--- 2DGT 2.7 Programming - Ujwal Semwal.py
+++ 2DGT 2.7 Programming - Ujwal Semwal.py
@@ -150,6 +150,7 @@ entry_receipt.pack(pady=2)
 
 tk.Label(root, text="Item Hired:", font=("Helvetica", 10, "bold"), bg="#eef2f5").pack(pady=(4, 0))
 entry_item = tk.Entry(root, width=40)
+entry_item.pack(pady=2)
 
 # this creates the start date box and listens for when the customer clicks it
 tk.Label(root, text="Date Hired From:", font=("Helvetica", 10, "bold"), bg="#eef2f5").pack(pady=(4, 0))
